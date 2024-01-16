7dcf9e79d840697f9bcdafd3e91f775ec5da5abd
HeJun <1255524587@qq.com>
2024-01-16 10:00:20 +0800

    框架定义

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import postcssPresetEnv from 'postcss-preset-env'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '#': path.resolve(__dirname, 'types')
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [postcssPresetEnv()]
+    }
+  },
+  server: {
+    host: '127.0.0.1'
+  }
+})
